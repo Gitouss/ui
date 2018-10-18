91d93bc3bd076a0ee4948c3e3d52c6aae9361e7f
hihebark <n.amara@protonmail.ch>
2018-10-19 00:57:47 +0100

    poopush

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './index/header/header.component';
+import { LayoutComponent } from './index/layout/layout.component';
+import { FooterComponent } from './index/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
